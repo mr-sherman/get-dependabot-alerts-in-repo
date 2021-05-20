f89b37b6ce26e7f65df820ee5e93238b3a6ab0e6
mr-sherman <77112096+mr-sherman@users.noreply.github.com>
2021-05-20 15:58:43 -0400

    changed this script to get code scanning from a single repo, not iterate over repos from an organization.

--- get-code-scanning-alerts.js
+++ get-code-scanning-alerts.js
@@ -12,42 +12,24 @@ const octokit = new Octokit({
 var buffer = ""
 
 const [, , ...args] = process.argv
-const owner = args[0]
+const repo_path = args[0]
 
-console.log("org,repo,tool,rule_id,severity,open,created_at,closed_by,closed_at,url,closed_reason")
+console.log("repo,tool,rule_id,severity,open,created_at,closed_by,closed_at,url,closed_reason")
 octokit
-  .paginate(octokit.repos.listForOrg, {
-      org: owner,
-    })
-  .then(repositories =>
-    pReduce(repositories, (repository) => {
-      if (repository.archived) {
-        return Promise.resolve();
-      }
-      const repo = repository.name
-
-      return octokit
-        .paginate("GET /repos/:owner/:repo/code-scanning/alerts?per_page=100", {
-          owner: owner,
-          repo: repo
+        .paginate("GET /repos/:repo/code-scanning/alerts?per_page=100", {
+          repo: repo_path
         })
         .then(alerts => {
           if (alerts.length > 0) {
 
             pReduce(alerts, (alert) => {
-              console.log(`${owner},${repo},${alert.tool.name},${alert.rule.id},${alert.rule.severity},${alert.state},${alert.created_at},${alert.dismissed_by},${alert.dismissed_at},${alert.html_url},${alert.dismissed_reason}`)
+              console.log(`${repo_path},${alert.tool.name},${alert.rule.id},${alert.rule.severity},${alert.state},${alert.created_at},${alert.dismissed_by},${alert.dismissed_at},${alert.html_url},${alert.dismissed_reason}`)
             }) 
           } 
           delay(300);
         })
         .catch(error => {
-         // console.error(`Failed for ${owner}/${repo}\n${error.message}\n${error.documentation_url}`)
+          console.error(`Getting repositories for repo ${repo_path} failed.
+          ${error.message} (${error.status})
+          ${error.documentation_url}`)
         })        
-    })
-    
-  )
-  .catch(error => {
-    console.error(`Getting repositories for organization ${owner} failed.
-    ${error.message} (${error.status})
-    ${error.documentation_url}`)
-  })
